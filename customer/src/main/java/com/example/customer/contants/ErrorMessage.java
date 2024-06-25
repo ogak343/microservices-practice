addeaecd737a3d76e27b3dd8ee2922667c3b0a30
khamd <khamdamov343@gmail.com>
2024-06-25 10:44:33 +0500

    master: customer some changes

--- customer/src/main/java/com/example/customer/contants/ErrorMessage.java
+++ customer/src/main/java/com/example/customer/contants/ErrorMessage.java
@@ -1,9 +1,18 @@
 package com.example.customer.contants;
 
+import lombok.Getter;
+
+@Getter
 public enum ErrorMessage {
-    WRONG_OTP_CODE,
-    WRONG_PASSWORD,
-    INVALID_TOKEN,
-    WRONG_CREDENTIALS,
-    OTP_EXPIRED
+    WRONG_OTP_CODE(400),
+    WRONG_PASSWORD(400),
+    INVALID_TOKEN(400),
+    WRONG_CREDENTIALS(400),
+    OTP_EXPIRED(400);
+
+    private int code;
+
+    ErrorMessage(int code) {
+        this.code = code;
+    }
 }
--- customer/src/main/java/com/example/customer/exception/CustomException.java
+++ customer/src/main/java/com/example/customer/exception/CustomException.java
@@ -3,7 +3,8 @@ package com.example.customer.exception;
 import com.example.customer.contants.ErrorMessage;
 
 public class CustomException extends RuntimeException {
-    public CustomException(ErrorMessage errorMessage) {
 
+    public CustomException(ErrorMessage errorMessage) {
+        super(errorMessage.name());
     }
 }
